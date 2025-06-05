e69168426522bd277a954e532855c7ff02683d2c
acevnn <ninoacev@gmail.com>
2025-06-05 10:47:58 +0200

    bugfix/fix-toast-notification-library-behaviour - Refactored the code slightly to prevent notification collisions

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import "./App.css";
-import TemporaryNotification from "./components/TemporaryNotification/TemporaryNotification";
-import ToastNotification from "./components/ToastNotification/ToastNotification";
+import './App.css';
+import TemporaryNotification from './components/TemporaryNotification/TemporaryNotification';
+import ToastNotification from './components/ToastNotification/ToastNotification';
+import {ToastContainer} from 'react-toastify';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <>
-      <section>
-        <ToastNotification
-          heading="Very important title of the notification"
-          paragraph="Notifications may extend two across lines. We prefer to keep them concise."
-        />
-        <TemporaryNotification />
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section>
+                <ToastNotification
+                    heading="Very important title of the notification"
+                    paragraph="Notifications may extend two across lines. We prefer to keep them concise."/>
+                <TemporaryNotification/>
+            </section>
+            <ToastContainer
+                position="bottom-right"
+                toastClassName="custom-toast"
+                closeButton={false}
+            />
+        </>
+    );
 }
 
 export default App;
